51fee73ef10d7de045cc8d8ac06752bcb11dfa51
Tantaii03 <taschai200303@gmail.com>
2026-02-18 11:35:35 +0700

    Commit02:เริ่มสร้างClassสำหรับแต่ละไฟล์ไว้แล้ว

---
+++ lib/models/cake_shop.dart
@@ -0,0 +1,29 @@
+class CakeShop {
+  String name;
+  String description;
+  String image1;
+  String image2;
+  String image3;
+  String address;
+  String phone;
+  String facebook;
+  String Website;
+  String openCloseTime;
+  double latitude;
+  double longitude;
+
+  CakeShop({
+    required this.name,
+    required this.description,
+    required this.image1,
+    required this.image2,
+    required this.image3,
+    required this.address,
+    required this.phone,
+    required this.facebook,
+    required this.Website,
+    required this.openCloseTime,
+    required this.latitude,
+    required this.longitude,
+  });
+}
---
+++ lib/view/cake_shop_list_ui.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CakeShopListUi extends StatefulWidget {
+  const CakeShopListUi({super.key});
+
+  @override
+  State<CakeShopListUi> createState() => _CakeShopListUiState();
+}
+
+class _CakeShopListUiState extends State<CakeShopListUi> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
